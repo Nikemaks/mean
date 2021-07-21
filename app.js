6abc8d6da67f45ec4cd704a121f5624693168672
Maksym Malyi <Maksym.Malyi@refinitiv.com>
2021-07-21 16:55:22 +0300

    add mongoDB and model

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
 const cors = require("cors");
 const morgan = require("morgan");
@@ -7,8 +8,12 @@ const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
+const keys = require('./config/keys');
 const app = express();
 
+mongoose.connect(keys.mongoURI)
+    .then(() => console.log('MongoDB connected.'))
+    .catch((e) => console.log(e));
 
 app.use(morgan('dev'));
 app.use(cors());
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://nikemaks:SiydzMlcg6aGuKgV@cluster0.cxbks.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
