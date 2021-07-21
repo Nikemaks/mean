97c081f8d2c9be485fcc8a1542afc510e286324f
Maksym Malyi <Maksym.Malyi@refinitiv.com>
2021-07-21 22:10:48 +0300

    login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,18 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
-module.exports.login = function (req, res) {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
-        }
-    })
+module.exports.login = async function (req, res) {
+    const candidate = await User.findOne({
+        email: req.body.email
+    });
+
+    if(candidate) {
+
+    } else {
+        res.status().json({
+
+        })
+    }
 }
 
 module.exports.register = async function (req, res) {
