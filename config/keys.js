6b50f42415df67b4628681777ec9e04b41c9f63c
Maksym Malyi <Maksym.Malyi@refinitiv.com>
2021-07-30 11:28:14 +0300

    add auth for application

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://nikemaks:SiydzMlcg6aGuKgV@cluster0.cxbks.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://nikemaks:SiydzMlcg6aGuKgV@cluster0.cxbks.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,32 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('./../config/keys');
 
 module.exports.login = async function (req, res) {
     const candidate = await User.findOne({
         email: req.body.email
     });
 
-    if(candidate) {
+    if (candidate) {
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+        if (passwordResult) {
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 3600});
 
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        } else {
+            res.status(401).json({
+                message: 'Пароли не совпадают. Попробуйте снова'
+            })
+        }
     } else {
-        res.status().json({
-
+        res.status(404).json({
+            message: 'Пользователь с таким email не найден'
         })
     }
 }
