70e132f7dbe92bf27f0b6b9f1ff258dcd24bea4e
Maksym Malyi <Maksym.Malyi@refinitiv.com>
2021-07-30 13:13:45 +0300

    Add category api

--- controllers/category.js
+++ controllers/category.js
@@ -1,15 +1,50 @@
-module.exports.getAll = (req, res) => {
-    res.json({message: 'Categories'})
-}
-module.exports.getById = (req, res) => {
+const errorHandler = require('../utils/errorHandler');
+const Category = require('../models/Category');
+const Position = require('../models/Position')
 
+module.exports.getAll = async (req, res) => {
+    try {
+        const categories = await Category.find({
+            user: req.user.id
+        })
+        res.status(200).json(categories);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
-module.exports.remove = (req, res) => {
+module.exports.getById = async (req, res) => {
+    try {
+        const category = await Category.findById(req.params.id);
+        res.status(200).json(category);
+    } catch (e) {
+        errorHandler(res, e);
+    }
+}
 
+module.exports.remove = async (req, res) => {
+    try {
+        await Category.remove({_id: req.params.id});
+        await Position.remove({category: req.params.id});
+        res.status(200).json({
+            message: 'Категория была удалена.'
+        });
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
+
 module.exports.create = (req, res) => {
+    try {
 
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
-module.exports.update = (req, res) => {
 
+module.exports.update = (req, res) => {
+    try {
+
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
