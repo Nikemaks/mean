967040bca8aca884395523f1531a1b34e2c007ff
Maksym Malyi <Maksym.Malyi@refinitiv.com>
2021-07-30 12:48:28 +0300

    Add error handler

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('./../config/keys');
+const errorHandler = require('../utils/errorHandler')
 
 module.exports.login = async function (req, res) {
     const candidate = await User.findOne({
@@ -54,7 +55,7 @@ module.exports.register = async function (req, res) {
             await user.save()
             res.status(201).json(user)
         } catch (e) {
-            console.log(e)
+            errorHandler(res, e);
         }
     }
 }
