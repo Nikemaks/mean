ef2b2e6fea08ef03a3af5089f309835d429519eb
Maksym Malyi <Maksym.Malyi@refinitiv.com>
2021-07-30 15:34:39 +0300

    Add creation category

--- routes/category.js
+++ routes/category.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const controller = require('../controllers/category');
-const passport = require('passport');
+const upload = require('../middleware/upload');
 
 router.get('/', passport.authenticate('jwt', {session: false}), controller.getAll);
-router.get('/:id', controller.getById);
-router.delete('/:id', controller.remove);
-router.post('/', controller.create);
-router.patch('/:id', controller.update);
+router.get('/:id', passport.authenticate('jwt', {session: false}), controller.getById);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), controller.remove);
+router.post('/', upload.single('image'), passport.authenticate('jwt', {session: false}), controller.create);
+router.patch('/:id', passport.authenticate('jwt', {session: false}), upload.single('image'), controller.update);
 
 
 module.exports = router;
