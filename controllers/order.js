4ac5988c9514204d12a03cbb924ff2f5f0a9c558
Maksym Malyi <Maksym.Malyi@refinitiv.com>
2021-08-15 14:13:27 +0300

    Add protections for routers

--- controllers/order.js
+++ controllers/order.js
@@ -1,6 +1,18 @@
-module.exports.getAll = (req, res) => {
+const errorHandler = require('../utils/errorHandler');
+const Order = require('../models/Order');
 
+module.exports.getAll = (req, res) => {
+    try {
+
+
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 module.exports.create = (req, res) => {
+    try {
 
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
