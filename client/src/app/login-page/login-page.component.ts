74abf5a5f0a79eeb13c94a0cf839edb19c43588a
Maksym Malyi <Maksym.Malyi@refinitiv.com>
2021-09-02 16:33:23 +0300

    add auth

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../shared/services/auth.service";
+import {Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  destroyed$: Subject<any> = new Subject<any>();
+
+  form: FormGroup = this.fb.group({
+    email: new FormControl(null, [Validators.email, Validators.required]),
+    password: new FormControl(null, [Validators.minLength(6), Validators.required])
+  });
+
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
 
   ngOnInit(): void {
+    this.route.queryParams
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((params: Params) => {
+        if (params['register']) {
+          // Теперь вы можете зайти используя свои данные
+        }
+        if (params['accessDenied']) {
+          // Необходима авторизация
+        }
+      });
   }
+
+  onSubmit() {
+    this.form.disable();
+
+    this.authService.login(this.form.value)
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(value => {
+        this.router.navigate(['/overview'])
+      }, error => {
+        console.warn(error);
+        this.form.enable();
+      })
+  }
+
 
 }
---
+++ client/src/app/shared/services/auth.service.ts
@@ -0,0 +1,47 @@
+import {Injectable} from '@angular/core';
+import {User} from "../interfaces";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {tap} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private token: any = null;
+
+  constructor(private http: HttpClient) {
+  }
+
+  register() {
+  }
+
+  login(user: User): Observable<{ token: string }> {
+    return this.http.post<{ token: string }>('/api/auth/login', user)
+      .pipe(
+        tap(
+          ({token}) => {
+            this.setToken(token);
+            localStorage.setItem('auth-token', token);
+          })
+      );
+  }
+
+  setToken(token: string | null) {
+    this.token = token;
+  }
+
+  getToken(): string {
+    return this.token;
+  }
+
+  isAuth(): boolean {
+    return !!this.token;
+  }
+
+  logout() {
+    this.setToken(null);
+    localStorage.clear();
+  }
+}
