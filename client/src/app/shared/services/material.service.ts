a503b8f83f075aedb5c9384cb4cee577c5dea1d9
Maksym Malyi <Maksym.Malyi@refinitiv.com>
2021-09-03 14:13:19 +0300

    add overview page

--- client/src/app/shared/services/material.service.ts
+++ client/src/app/shared/services/material.service.ts
@@ -1,8 +1,14 @@
+import {ElementRef} from "@angular/core";
+
 declare var M: any;
 
 export class MaterialService {
   static toast(message: string) {
     M.toast({html: message})
+  }
+
+  static initializeFloatingButton(ref: ElementRef | undefined) {
+    M.FloatingActionButton.init(ref?.nativeElement);
   }
 
 }
