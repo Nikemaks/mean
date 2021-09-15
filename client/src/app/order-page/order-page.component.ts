3cc780eb02ab14a29d3dee06dae4017d3e86946f
Maksym Malyi <Maksym.Malyi@refinitiv.com>
2021-09-15 23:09:15 +0300

    add order page and service

--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
+import {MaterialInstance, MaterialService} from "../shared/services/material.service";
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
   styleUrls: ['./order-page.component.css']
 })
-export class OrderPageComponent implements OnInit {
+export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('modal') modalRef!: ElementRef;
+  modal!: MaterialInstance;
 
-  ngOnInit(): void {
+  isRoot: boolean = true;
+
+  constructor(private router: Router) {
   }
 
+  ngAfterViewInit(): void {
+    this.modal = MaterialService.initModal(this.modalRef);
+  }
+
+  ngOnDestroy(): void {
+    this.modal.destroy();
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isRoot = this.router.url === '/order';
+      }
+    })
+  }
+
+  open() {
+    this.modal.open();
+  }
+
+  cancel() {
+    this.modal.close();
+  }
+
+  submit() {
+    this.modal.close();
+  }
 }
---
+++ client/src/app/shared/services/order.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  constructor() {
+  }
+
+  add() {
+
+  }
+
+  remove() {
+  }
+
+  clear() {
+  }
+}
