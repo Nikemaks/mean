d661c0ecb93a93fc593196ea9303fbb151379812
Maxim Malyi <malyi.1992@mail.ru>
2021-09-19 12:18:38 +0300

    overview page

--- client/src/app/overview-page/overview-page.component.ts
+++ client/src/app/overview-page/overview-page.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AnalyticsService} from "../shared/services/analytics.service";
+import {Observable} from "rxjs/internal/Observable";
+import {OverviewPage} from "../shared/interfaces";
+import {MaterialInstance, MaterialService} from "../shared/services/material.service";
 
 @Component({
   selector: 'app-overview-page',
   templateUrl: './overview-page.component.html',
   styleUrls: ['./overview-page.component.css']
 })
-export class OverviewPageComponent implements OnInit {
+export class OverviewPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('tapTarget') tapTargetRef!: ElementRef;
 
-  ngOnInit(): void {
+  tapTarget!: MaterialInstance;
+
+  constructor(private overviewService: AnalyticsService) {
   }
 
+  data$: Observable<OverviewPage> = this.overviewService.getOverview();
+
+  yesterday: Date = new Date();
+
+  ngOnInit(): void {
+    this.yesterday.setDate(this.yesterday.getDate() - 1);
+
+  }
+
+  ngAfterViewInit(): void {
+    this.tapTarget = MaterialService.initTapTarget(this.tapTargetRef);
+  }
+
+  ngOnDestroy(): void {
+    this.tapTarget.destroy();
+  }
+
+
+  openInfo() {
+    this.tapTarget.open();
+  }
 }
--- controllers/analytics.js
+++ controllers/analytics.js
@@ -1,26 +1,35 @@
-const moment = require('moment');
-const Order = require('../models/Order');
-const errorHandler = require('../utils/errorHandler');
+const moment = require('moment')
+const Order = require('../models/Order')
+const errorHandler = require('../utils/errorHandler')
 
-module.exports.overview = async (req, res) => {
-
+module.exports.overview = async function(req, res) {
     try {
-        const allOrders = await Order.find({user: req.user.id}).sort(1);
+        const allOrders = await Order.find({user: req.user.id}).sort({date: 1});
         const ordersMap = getOrdersMap(allOrders);
-        const yesterdayOrders = ordersMap[moment().add(-1, 'd').format('DD.MM.YYYY')] || [];
+        const yesterdayOrders = ordersMap[moment().add(-1, 'd').format('DD.MM.YYYY')] || []
 
+        // Количество заказов вчера
         const yesterdayOrdersNumber = yesterdayOrders.length;
-        const totalOrdersNumber = ordersMap.length;
+        // Количество заказов
+        const totalOrdersNumber = allOrders.length;
+        // Количество дней всего
         const daysNumber = Object.keys(ordersMap).length;
+        // Заказов в день
         const ordersPerDay = (totalOrdersNumber / daysNumber).toFixed(0);
-        const ordersPercent = (((yesterdayOrdersNumber / ordersPerDay) - 1) * 100).toFixed(2);
+        // ((заказов вчера \ кол-во заказов в день) - 1) * 100
+        // Процент для кол-ва заказов
+        const ordersPercent = (((yesterdayOrdersNumber / ordersPerDay) - 1) * 100).toFixed(2)
+        // Общая выручка
         const totalGain = calculatePrice(allOrders);
+        // Выручка в день
         const gainPerDay = totalGain / daysNumber;
+        // Выручка за вчера
         const yesterdayGain = calculatePrice(yesterdayOrders);
+        // Процент выручки
         const gainPercent = (((yesterdayGain / gainPerDay) - 1) * 100).toFixed(2);
-
+        // Сравнение выручки
         const compareGain = (yesterdayGain - gainPerDay).toFixed(2);
-
+        // Сравнение кол-ва заказов
         const compareNumber = (yesterdayOrdersNumber - ordersPerDay).toFixed(2);
 
         res.status(200).json({
@@ -33,18 +42,17 @@ module.exports.overview = async (req, res) => {
             orders: {
                 percent: Math.abs(+ordersPercent),
                 compare: Math.abs(+compareNumber),
-                yesterday: +yesterdayOrders,
+                yesterday: +yesterdayOrdersNumber,
                 isHigher: +ordersPercent > 0
             }
         })
 
-
     } catch (e) {
-        errorHandler(e);
+        errorHandler(res, e)
     }
 };
 
-module.exports.analytics = (req, res) => {
+module.exports.analytics = function(req, res) {
 
 };
 
@@ -54,26 +62,23 @@ function getOrdersMap(orders = []) {
         const date = moment(order.date).format('DD.MM.YYYY');
 
         if (date === moment().format('DD.MM.YYYY')) {
-            return;
+            return
         }
 
         if (!daysOrders[date]) {
-            daysOrders[date] = [];
+            daysOrders[date] = []
         }
 
-        daysOrders[date].push(order);
+        daysOrders[date].push(order)
     });
-
-    return daysOrders;
-
+    return daysOrders
 }
 
 function calculatePrice(orders = []) {
-    return orders.reduce((total, curr) => {
-        const orderPrice = curr.list.reduce((orderTotal, item) => {
-            return orderTotal += item.cost * item.quantity;
-        });
-
-        return total += orderPrice;
-    }, 0);
+    return orders.reduce((total, order) => {
+        const orderPrice = order.list.reduce((orderTotal, item) => {
+            return orderTotal += item.cost * item.quantity
+        }, 0);
+        return total += orderPrice
+    }, 0)
 }
